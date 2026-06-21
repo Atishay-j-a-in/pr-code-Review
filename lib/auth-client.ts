81e12f3d2ba50d628aeed91ec53054a02c7a4969
Atishay-j-a-in <shepherdk450@gmail.com>
2026-06-21 19:45:24 +0530

    feat: add GithubInstallation model and migration for GitHub app integration

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,8 +2,4 @@ import { createAuthClient } from "better-auth/react"
 
 
 export const authClient = createAuthClient()
-const signIn = async () => {
-    const data = await authClient.signIn.social({
-        provider: "github"
-    })
-}
+
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,18 +1,23 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
 import { prisma } from "./db";
 import { nextCookies } from "better-auth/next-js";
+
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
-    plugins: [nextCookies()],
     socialProviders: { 
     github: { 
       clientId: process.env.GITHUB_CLIENT_ID!, 
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!, 
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      
+      mapProfileToUser:async (profile) => ({
+        email: profile.email?? `${profile.login}@github.com`,
+        name: profile.name?? profile.login,
+      })
     }
-}
+   },
+   plugins: [nextCookies()],
 });
